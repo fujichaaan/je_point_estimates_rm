f7c6afa7175fe59235b815e616ecbe8d8f6b5b6b
Ryosuke FUJII <ryosukefujii0320@gmail.com>
2023-03-07 16:37:35 +0100

    Add files via upload

--- code_figures.R
+++ code_figures.R
@@ -1,27 +1,24 @@
-if (!requireNamespace("pacman", quietly = TRUE)) {
-  install.packages("pacman")
-}
-
-pacman::p_load(tidyverse)
+install.packages("tidyverse")
+library(tideyverse)
 
 # Data preparation --------------------------------------
 
-odds_ratio <- data.frame(ID    = c(1, 2),
-                         Class = c("A", "B"),
-                         OR    = c(0.5, 2.0),
-                         Start = c(1.0, 1.0),
-                         End   = c(0.5, 2.0),
-                         Upper = c(0.9, 2.8),
-                         Lower = c(0.35, 1.5))
+table <- data.frame(ID    = c(1, 2),
+                    Class = c("A", "B"),
+                    RR    = c(0.5, 2.0),
+                    Start = c(1.0, 1.0),
+                    End   = c(0.5, 2.0),
+                    Upper = c(0.87, 2.8),
+                    Lower = c(0.35, 1.5))
 
 # Figure 1 Panel A --------------------------------------
 
-ggplot(odds_ratio, aes(x = Class, y = OR)) +
+ggplot(table, aes(x = Class, y = RR)) +
   geom_bar(stat = "identity", fill = "Black") +
   geom_errorbar(aes(ymin = Lower, ymax = Upper), 
                 width = .2,
                 position = position_dodge(.9)) +
-  labs(y = "Odds ratio (95% CI)") +
+  labs(y = "Risk ratio (95% CI)") +
   scale_y_continuous(limits = c(0, 3),
                      breaks = seq(0, 3, 0.5)) +
   theme_bw() +
@@ -34,13 +31,13 @@ ggplot(odds_ratio, aes(x = Class, y = OR)) +
 # Figure 1 Panel B --------------------------------------
 
 
-ggplot(odds_ratio, aes(x = Class)) +
+ggplot(table, aes(x = Class)) +
   geom_rect(aes(x = Class, xmin = ID - 0.45, xmax = ID + 0.45,
                 ymin = End, ymax = Start), fill = "Black") +
   geom_errorbar(aes(ymin = Lower, ymax = Upper), 
                 width = .2,
                 position = position_dodge(.9)) +
-  labs(y = "Odds ratio (95% CI)") +
+  labs(y = "Risk ratio (95% CI)") +
   scale_y_continuous(limits = c(0, 3),
                      breaks = seq(0, 3, 0.5)) +
   theme_bw() +
@@ -53,15 +50,16 @@ ggplot(odds_ratio, aes(x = Class)) +
 
 # Figure 1 Panel C --------------------------------------
 
-ggplot(odds_ratio) +
+ggplot(table) +
   geom_rect(aes(x = Class, xmin = ID - 0.45, xmax = ID + 0.45,
                 ymin = End, ymax = Start), fill = "Black") +
   geom_errorbar(aes(x = Class, ymin = Lower, ymax = Upper), 
                 width = .2,
                 position = position_dodge(.9)) +
-  labs(y = "Odds ratio (95% CI)") +
-  scale_y_log10(limits = c(0.1, 3),
-                breaks = c(0.25, 0.5, 1.0, 2.0, 3.0)) +
+  labs(y = "Risk ratio (95% CI)") +
+  scale_y_continuous(limits = c(0.1, 3),
+                     breaks = c(0.25, 0.5, 1.0, 2.0, 3.0),
+                     trans = "log10") +
   theme_bw() +
   theme(axis.title = element_text(size = 24),
         axis.text = element_text(size = 20),
@@ -72,18 +70,20 @@ ggplot(odds_ratio) +
 
 # Figure 1 Panel D --------------------------------------
 
-ggplot(odds_ratio) +
+ggplot(table) +
   geom_hline(yintercept = 1, linetype = 2) +
-  geom_pointrange(aes(x = Class, y = OR, 
+  geom_pointrange(aes(x = Class, y = RR, 
                       ymin = Lower, ymax = Upper),
                   size = 2, linewidth = 2,
                   position = position_dodge(0.85)) +
-  labs(y = "Odds ratio (95% CI)") +
-  scale_y_log10(limits = c(0.1, 3),
-                breaks = c(0.25, 0.5, 1.0, 2.0, 3.0)) +
+  labs(y = "Risk ratio (95% CI)") +
+  scale_y_continuous(limits = c(0.1, 3),
+                     breaks = c(0.25, 0.5, 1.0, 2.0, 3.0),
+                     trans = "log10") +
   theme_bw() +
   theme(axis.title = element_text(size = 24),
         axis.text = element_text(size = 20),
         axis.title.x = element_blank(),
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank())
+
---
+++ code_practices.R
@@ -0,0 +1,172 @@
+# Practical case 1 --------------------------------------------------------
+# Perform multivariable logistic regression to estimate odds ratio and plot it as points + error bars
+
+install.packages("causaldata")
+library(causaldata)
+
+# Peforming logistic regression
+data(nhefs_complete)
+fit <- glm(death ~ sex + age + sbp + tumor, data = nhefs_complete, family = binomial)
+summary(fit)
+
+# Creating data frame for data visualization
+table2 <- data.frame(OR  = exp(summary(fit)$coefficients[-1,1]),
+                     UCI = exp(confint(fit, level = .95)[-1,1]),
+                     LCI = exp(confint(fit, level = .95)[-1,2]))
+
+rownames(table2) <- c("SEX_Female",
+                      "Age",
+                      "SBP",
+                      "Cancer")
+
+# Visualization
+install.packages("forplo")
+library(forplo)
+
+forplo(table2[,1:3],
+       sort        = TRUE,
+       favorlabs   = c("Lower mortality", "Higher mortality"),
+       size        = 3,
+       ci.edge     = FALSE,
+       ci.lwd      = 2,
+       char        = 20,
+       left.bar    = FALSE, 
+       row.labels  = c("SEX_Female",
+                       "Age",
+                       "SBP",
+                       "Cancer"),
+       title       = "Logistic regression, sorted by OR")
+
+
+
+# Practical case 2 --------------------------------------------------------
+# Summarize different studies and plot it as forest plot
+# Example from the forplo package 
+
+# Creating original data
+forest <- data.frame(OR     = c(1.21,0.90,1.02,
+                                1.54,1.32,0.79,1.38,0.85,1.11,
+                                1.58,1.80,2.27),
+                     LCI    = c(0.82,0.61,0.66,
+                                1.08,0.91,0.48,1.15,0.39,0.91,
+                                0.99,1.48,0.92),
+                     UCI    = c(1.79,1.34,1.57,
+                                2.19,1.92,1.32,1.64,1.87,1.34,
+                                2.54,2.19,5.59),
+                     groups=c(1,1,1,
+                              2,2,2,2,2,2,
+                              3,3,3))
+
+
+rownames(forest) <- c('Barry, 2005', 'Frances, 2000', 'Rowley, 1995',
+                      'Biro, 2000', 'Crowe, 2010', 'Harvey, 1996',
+                      'Johns, 2004', 'Parr, 2002', 'Zhang, 2011',
+                      'Flint, 1989', 'Mac Vicar, 1993', 'Turnbull, 1996')
+
+
+# Calculating weights and overall results
+logOR <- round(log(forest$OR), 2)
+SE <- round((log(forest$UCI) - logOR) / 1.96, 2)
+metatot <- meta::metagen(logOR, SE)
+
+forest$logOR <- logOR
+forest$SE <- SE
+forest$weights <- round(metatot$w.random / sum(metatot$w.random) * 100, 2)
+
+forest <- rbind(forest,
+                c(round(exp(metatot$TE.random), 2),
+                  round(exp(metatot$lower.random), 2),
+                  round(exp(metatot$upper.random), 2),
+                  4,
+                  round(metatot$TE.random, 2),
+                  round(metatot$seTE.random, 2),
+                  100))
+
+rownames(forest)[13] <- "All"
+
+
+# Data visualization for meta-analsys
+w <- forest$weights
+w[13] <- NA
+
+forplo(forest[,1:3],
+       groups       = forest$groups,
+       grouplabs    = c('Low risk of bias',
+                        'Some concerns',
+                        'High risk of bias',
+                        'Overall'),
+       left.align   = TRUE,
+       ci.edge      = FALSE,
+       ci.lwd       = 2,
+       char         = 15,
+       left.bar     = FALSE, 
+       diamond      = 13,
+       diamond.col  = "Grey",
+       scaledot.by  = w)
+
+
+# Practical case 3 --------------------------------------------------------
+# Perform logistic regression with spline and plot it
+
+install.packages("rms")
+library(rms)
+
+data(nhefs_complete)
+nhefs_complete1 <- nhefs_complete |>
+  drop_na(sbp)
+
+# Setting knots
+ddist <- datadist(nhefs_complete1)
+options(datadist = 'ddist')
+k <- with(nhefs_complete1, quantile(sbp, c(.05, 0.25, .75, .95)))
+
+# Logistic regression
+summary2 <- lrm(death ~ sex + age + rcs(sbp, k) + tumor, data = nhefs_complete1)
+
+# Change SBP = 102 as a reference
+ddist$limits["Adjust to", "sbp"] <- 120
+summary3 <- update(summary2)
+
+
+## Pattern 1: Liner scale for non-linear association #####################
+
+table3 <- Predict(summary3, sbp, ref.zero = TRUE, fun = exp)
+
+ggplot(table3, aes(sbp, yhat)) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
+  scale_y_continuous(limits = c(0, 6),
+                     breaks = seq(0, 6, 1)) +
+  scale_x_continuous(limits = c(90, 190),
+                     breaks = seq(100, 180, 20)) +
+  labs(x = "SBP, mmHg",
+       y = "Odds ratio (95% CI)") +
+  theme_bw() +
+  theme(axis.line = element_line(size = 0.5, linetype = "solid"),
+        panel.grid.major = element_line(colour = "gray98"),
+        axis.title = element_text(size = 24),
+        axis.text = element_text(size = 20),
+        plot.caption = element_blank(),
+        panel.background = element_rect(fill = "gray99",
+                                        colour = "white", linetype = "twodash"),
+        plot.background = element_rect(fill = "white"))
+
+
+## Pattern 2: Logarithmic scale for non-linear association #####################
+
+table4 <- Predict(summary3, sbp, ref.zero = TRUE)
+
+ggplot(table4, aes(sbp, yhat)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  scale_x_continuous(limits = c(90, 190),
+                     breaks = seq(100, 180, 20)) +
+  labs(x = "SBP, mmHg",
+       y = "Log-transformed odds ratio (95% CI)") +
+  theme_bw() +
+  theme(axis.line = element_line(size = 0.5, linetype = "solid"),
+        panel.grid.major = element_line(colour = "gray98"),
+        axis.title = element_text(size = 24),
+        axis.text = element_text(size = 20),
+        plot.caption = element_blank(),
+        panel.background = element_rect(fill = "gray99",
+                                        colour = "white", linetype = "twodash"),
+        plot.background = element_rect(fill = "white"))
